632fbf2e641954f955264b4d44ed2105d4e95b03
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-02-26 19:47:23 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'. $postId ." Komentar ke-: ". $commentId;
 });
