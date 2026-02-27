735e87306e5a5605948bcf580b3b7d5f1c61614c
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-02-27 11:37:44 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'. $postId ." Komentar ke-: ". $commentId;
-=======
-Route::get('/hello', function () {
-    return 'Hello World';
->>>>>>> aeebc038ca235fd7f95f764971815bfaf6ea925c
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Sileysa Faedatul Nuraini '. $name;
 });
